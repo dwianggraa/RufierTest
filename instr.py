2048c8578ee0fb289e83627153295ae29f86d179
Dwi Anggraeni <94098023+dwianggraa@users.noreply.github.com>
2026-02-06 11:43:59 +0700

    Create instr.py
    text section

---
+++ instr.py
@@ -0,0 +1,46 @@
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Selamat datang di program deteksi status kesehatan!'
+txt_next = 'Mulai'
+
+txt_instruction = ('Aplikasi ini memungkinkan Anda menggunakan tes Rufier untuk membuat diagnosis awal kondisi kesehatan Anda.\n'
+                   'Tes Rufier adalah serangkaian latihan fisik yang dirancang untuk menilai kinerja jantung selama aktivitas fisik.\n'
+                   'Subjek berbaring terlentang selama 5 menit dan denyut nadinya diukur selama 15 detik; \n'
+                   'kemudian, dalam waktu 45 detik, subjek melakukan 30 kali squat.\n'
+                   'Setelah latihan selesai, subjek berbaring dan denyut nadinya diukur kembali selama 15 detik pertama\n'
+                   'dan kemudian selama 15 detik terakhir pada menit pertama masa pemulihan.\n'
+                   'Penting! Jika Anda merasa tidak sehat selama tes (pusing,\n'
+                   'telinga berdenging, sesak napas, dll.), hentikan tes dan konsultasikan dengan dokter.')
+
+txt_title = 'Kesehatan'
+txt_name = 'Masukkan nama lengkap Anda:'
+txt_hintname = "Nama lengkap"
+txt_hintage = "0"
+
+txt_test1 = 'Berbaringlah terlentang dan hitung denyut nadi Anda selama 15 detik. Klik tombol "Mulai tes pertama" untuk memulai timer.\nTuliskan hasilnya pada kolom yang sesuai.'
+txt_test2 = 'Lakukan 30 kali squat dalam 45 detik. Untuk melakukannya, klik tombol "Mulai squat"\nuntuk memulai penghitung squat.'
+txt_test3 = 'Berbaringlah terlentang dan hitung denyut nadi selama 15 detik pertama pada menit tersebut, lalu selama 15 detik terakhir pada menit tersebut.\nTekan tombol "Mulai tes akhir" untuk memulai timer.\nDetik yang harus diukur ditandai dengan warna hijau dan menit yang tidak perlu diukur ditandai dengan warna hitam. Tuliskan hasilnya pada kolom yang sesuai.'
+
+txt_sendresults = 'Kirim hasil'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+
+txt_starttest1 = 'Mulai tes pertama'
+txt_starttest2 = 'Mulai squat'
+txt_starttest3 = 'Mulai tes akhir'
+txt_timer = ''
+
+txt_age = 'Usia (tahun penuh):'
+txt_finalwin = 'Hasil'
+txt_index = 'Indeks Rufier: '
+txt_workheart = 'Kinerja jantung: '
+
+txt_index = 'Indeks Roufier: '
+txt_workheart = 'Kinerja jantung: '
+txt_res1 = "rendah. Temui dokter Anda segera!"
+txt_res2 = "memuaskan. Temui dokter Anda!"
+txt_res3 = "rata-rata. Mungkin ada baiknya menemui dokter Anda untuk diperiksa."
+txt_res4 = "di atas purata"
+txt_res5 = "tinggi"
